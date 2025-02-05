f9c6f54bb23e1f9b41631f46f2bf0a531bd49bca
Lily Conrad <193945173+conradlml@users.noreply.github.com>
2025-02-05 12:10:29 -0800

    Uploading script file.

---
+++ get_StreamCat.R
@@ -0,0 +1,110 @@
+
+################################################################################
+# Direct watershed and catchment data download from EPA's StreamCat Dataset. 
+# 
+# Lily Conrad, IDEQ State Office
+# last update: 1/3/2025
+# 
+# Data citation: 
+# Weber, Marc H, Hill, Ryan A., Brookes, Allen F. 2024, StreamCatTools: Tools to 
+# work with the StreamCat API within R and access the full suite of StreamCat and 
+# LakeCat metrics, https://usepa.github.io/StreamCatTools
+################################################################################
+
+################################################################################
+#                              READ ME!
+################################################################################
+
+### Background information -----------------------------------------------------
+
+# EPA's StreamCat Dataset contains over 600 metrics characterizing watersheds
+# and catchments. Most of these values were derived via modeling and include
+# predicted water temperature, predicted biological condition, landscape 
+# conditions and much more. For a full description of the dataset, visit EPA's 
+# website (https://www.epa.gov/national-aquatic-resource-surveys/streamcat-dataset). 
+# To download StreamCat data, you can either visit their website or use this R 
+# script. 
+
+# This script was developed to download water column data from a user provided
+# HUC. The query can be adjusted slightly to expand the search to groundwater
+# or even sediment. 
+
+# To get started, provide the user inputs below following the outlined format.
+# Please review the instructions and examples carefully, the format matters. 
+
+
+### User inputs ----------------------------------------------------------------
+
+# Identify your area of interest. See this link for descriptions of the options:
+# https://www.epa.gov/national-aquatic-resource-surveys/streamcat-dataset-readme#geospatial
+# If you want to enter multiple, separate them by commas, but include all entries
+# in the same pair of quotation marks and with no spaces between them.  
+aoi <- "catchment"
+ 
+# Run this line of code. 
+parameter_codes <- sc_get_params(param='name') %>% as.data.frame()
+
+# You should see an item appear with the name "parameter_codes" under your Global
+# Environment (top right window). Click on it to open it in a new tab. Then 
+# Check go to this website: https://www.epa.gov/national-aquatic-resource-surveys/streamcat-metrics-and-definitions). 
+# Check the information on the website to help you identify which parameters you
+# would like to download then enter your parameters of interest below. Make 
+# sure that the words you enter match up with the valid codes from the parameter_code
+# table you created in R. 
+# If you want to enter multiple, separate them by commas, but include all entries
+# in the same pair of quotation marks and with no spaces between them.  
+# Enter your parameters of interest. 
+metric <- "rddens,septic,WettedWidth,BankfullWidth,precip9120,runoff,pcturbhi2019"
+
+# If you know which county/counties your watershed is located in, enter it below.
+# Enter the FIPS code, not the name (https://www.weather.gov/media/pimar/FIPS/id_cnty.pdf). 
+# If you want to enter multiple, separate them by commas, but include all entries
+# in the same pair of quotation marks and with no spaces between them. 
+county <- "16059,16037"
+
+# Enter your username (the name at the beginning of your computer's file explorer
+# path) in quotations.
+my_name <- "lconrad"
+
+
+# Great! Now that you've entered the values above, click on "Source" and an 
+# Excel file will save in your Downloads folder. If you get a message
+# asking you if you want to restart your R session, say no. If your console 
+# stops running code and asking for a number, type 1 and hit enter. After the file
+# downloads successfully, you can now join this with an NHDPlus dataset by the 
+# COMID in R or in ArcGIS software. 
+
+
+################################################################################
+#                                 START
+################################################################################
+
+### Load packages and data -----------------------------------------------------
+
+my_packages <- c("dplyr", "openxlsx", "remotes")
+install.packages(my_packages, repos = "http://cran.rstudio.com")
+
+library(dplyr)
+library(openxlsx)
+library(remotes)
+install_github("USEPA/StreamCatTools", build_vignettes=FALSE) # most recent approved version
+library(StreamCatTools)
+
+
+### Retrieve data --------------------------------------------------------------
+
+# Data query.            
+sc.dat <- sc_get_data(aoi = aoi,
+                      metric = metric,
+                      county = county,
+                      state = 'ID')
+
+
+# Save the data to excel. This will save the file in your downloads folder
+# Adjust the file path if you'd like it to save somewhere else.
+write.xlsx(sc.dat, paste0("C:/Users/",my_name,"/Downloads/StreamCat_download_",Sys.Date(),".xlsx"))
+
+
+################################################################################
+#                                 END
+################################################################################
